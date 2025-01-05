58f4f14b2150027aa3ddeaa7b3fad4dd99736d66
Micci <micci@micci.com>
2025-01-05 17:47:18 -0300

    (refact): split enum to a class

--- python-playground/src/domain/model/usuario/usuario.py
+++ python-playground/src/domain/model/usuario/usuario.py
@@ -1,27 +1,9 @@
-from src.domain.model.exception.customException import *
-from enum import Enum
-
-
-class UsuarioValidacoes(Enum):
-    NOME_INVALIDO = "Nome Invalido!"
-    NOME_NAO_PODE_TER_NUMEROS = "Nome Invalido! Não pode conter números!"
-    NOME_NAO_PODE_TER_CARACTERES_ESPECIAIS = "Nome Invalido! Nome pode conter caracteres especiais!"
-    NOME_NAO_PODE_SER_MENOR_QUE_3_CHAR = "Nome Invalido! Nome deve ter pelo menos 3 caracteres"
-    NOME_PRECISA_SER_STRING = "Nome invalido! Nome precisa ser do tipo String"
-
-    EMAIL_NAO_PODE_COMECAR_OU_TERMINAR_COM_CARACTERES_ESPECIAIS = "Email Invalido! Email nao pode comecar ou terminar com caracteres especiais!"
-    EMAIL_DEVE_CONTER_ARROBA = "Email Invalido! Email deve conter @"
-    EMAIL_NAO_PODE_CONTER_MAIS_QUE_UM_ARROBA = "Email Invalido! Email nao pode conter mais de um @!"
-    EMAIL_DEVE_CONTER_PONTOS_APOS_ARROBA = "Email Invalido! Email deve conter ponto apos o @!"
-    EMAIL_DEVE_TER_PELO_MENOS_2_CARACTERES_ANTES_ARROBA = "Email Invalido! Email deve conter mais de 2 caracteres antes do @!"
-    EMAIL_DEVE_TER_PELO_MENOS_2_CARACTERES_DEPOIS_ARROBA = "Email Invalido! Email deve conter mais de 2 caracteres depois do @!"
-
-    CARACTERES_ESPECIAIS = "@#$%^&*()-+?_=,<>!/."
-
+from domain.model.exception.customException import *
+from domain.model.usuario.enumUsuario import UsuarioEnum
 
 class Usuario():
 
-    __caracter_especiais = UsuarioValidacoes.CARACTERES_ESPECIAIS.value
+    __caracter_especiais = UsuarioEnum.CARACTERES_ESPECIAIS.value
 
     def __init__(self, nome: str, email: str) -> None:
 
@@ -34,26 +16,25 @@ class Usuario():
     def __checa_se_nome_valido(self, nome: str) -> bool:
         lista_erros = []
         if not nome:
-            lista_erros.append(UsuarioValidacoes.NOME_INVALIDO.value)
+            lista_erros.append(UsuarioEnum.NOME_INVALIDO.value)
         # verify if nome is a string
         if not isinstance(nome, str):
-            lista_erros.append(UsuarioValidacoes.NOME_PRECISA_SER_STRING.value)
+            lista_erros.append(UsuarioEnum.NOME_PRECISA_SER_STRING.value)
             raise NomeInvalidoError(lista_erros)
 
         if isinstance(nome, str) and any(char.isdigit() for char in nome):
             lista_erros.append(
-                UsuarioValidacoes.NOME_NAO_PODE_TER_NUMEROS.value)
+               UsuarioEnum.NOME_NAO_PODE_TER_NUMEROS.value)
         # nome cannot have special characters
         if any(char in self.__caracter_especiais for char in nome):
             lista_erros.append(
-                UsuarioValidacoes.NOME_NAO_PODE_TER_CARACTERES_ESPECIAIS.value)
+                UsuarioEnum.NOME_NAO_PODE_TER_CARACTERES_ESPECIAIS.value)
         # nome needs to be at least 3 characters long
         if len(nome) < 3:
             lista_erros.append(
-                UsuarioValidacoes.NOME_NAO_PODE_SER_MENOR_QUE_3_CHAR.value)
+                UsuarioEnum.NOME_NAO_PODE_SER_MENOR_QUE_3_CHAR.value)
 
         if len(lista_erros) > 0:
-            print(lista_erros)
             raise NomeInvalidoError(lista_erros)
         return True
 
@@ -65,28 +46,28 @@ class Usuario():
         # check se o email começa ou termina com carcter especiais
         if email[0] in character_especiais or email[-1] in character_especiais:
             lista_erros.append(
-                UsuarioValidacoes.EMAIL_NAO_PODE_COMECAR_OU_TERMINAR_COM_CARACTERES_ESPECIAIS.value)
+                UsuarioEnum.EMAIL_NAO_PODE_COMECAR_OU_TERMINAR_COM_CARACTERES_ESPECIAIS.value)
         # check se o email possui @
         if "@" not in email:
             lista_erros.append(
-                UsuarioValidacoes.EMAIL_DEVE_CONTER_ARROBA.value)
+                UsuarioEnum.EMAIL_DEVE_CONTER_ARROBA.value)
         # check se email possuim mais de um @
         else:
             if email.count("@") > 1:
                 lista_erros.append(
-                    UsuarioValidacoes.EMAIL_NAO_PODE_CONTER_MAIS_QUE_UM_ARROBA.value)
+                    UsuarioEnum.EMAIL_NAO_PODE_CONTER_MAIS_QUE_UM_ARROBA.value)
             # check se o email tem um ponto apos o arroba
             if "." not in email.split("@")[1]:
                 lista_erros.append(
-                    UsuarioValidacoes.EMAIL_DEVE_CONTER_PONTOS_APOS_ARROBA.value)
+                    UsuarioEnum.EMAIL_DEVE_CONTER_PONTOS_APOS_ARROBA.value)
              # check se email possui mais de 3 caracteres antes do arroba
             if len(email.split("@")[0]) < 3:
                 lista_erros.append(
-                    UsuarioValidacoes.EMAIL_DEVE_TER_PELO_MENOS_2_CARACTERES_ANTES_ARROBA.value)
+                    UsuarioEnum.EMAIL_DEVE_TER_PELO_MENOS_2_CARACTERES_ANTES_ARROBA.value)
             # check se email possui mais de 3 caracteres depois do arroba
             if len(email.split("@")[1]) < 3:
                 lista_erros.append(
-                    UsuarioValidacoes.EMAIL_DEVE_TER_PELO_MENOS_2_CARACTERES_DEPOIS_ARROBA.value)
+                    UsuarioEnum.EMAIL_DEVE_TER_PELO_MENOS_2_CARACTERES_DEPOIS_ARROBA.value)
 
         if len(lista_erros) > 0:
             raise EmailInvalidoError(lista_erros)
--- python-playground/tests/domain/model/usuario/usuario_test.py
+++ python-playground/tests/domain/model/usuario/usuario_test.py
@@ -1,5 +1,6 @@
-from src.domain.model.usuario.usuario import Usuario, UsuarioValidacoes
-from src.domain.model.exception.customException import *
+from domain.model.usuario.enumUsuario import UsuarioEnum
+from domain.model.usuario.usuario import Usuario
+from domain.model.exception.customException import *
 import pytest
 
 
@@ -19,41 +20,41 @@ class TestUsuario():
         with pytest.raises(NomeInvalidoError) as target:
             self.criar_usuario(nome)
         print(str(target.value))
-        assert UsuarioValidacoes.NOME_INVALIDO.value in target.value.messages
+        assert UsuarioEnum.NOME_INVALIDO.value in target.value.messages
 
     @pytest.mark.parametrize("nome", [123, 123.0])
     def test_nome_precisa_ser_string(self, nome):
         with pytest.raises(NomeInvalidoError) as target:
             self.criar_usuario(nome)
-        assert UsuarioValidacoes.NOME_PRECISA_SER_STRING.value in target.value.messages
+        assert UsuarioEnum.NOME_PRECISA_SER_STRING.value in target.value.messages
 
     @pytest.mark.parametrize("nome", ["Mic13ci", "Luiz123", "123"])
     def test_nome_nao_pode_conter_numeros(self, nome):
         with pytest.raises(NomeInvalidoError) as target:
             self.criar_usuario(nome)
-        assert UsuarioValidacoes.NOME_NAO_PODE_TER_NUMEROS.value in target.value.messages
+        assert UsuarioEnum.NOME_NAO_PODE_TER_NUMEROS.value in target.value.messages
 
     @pytest.mark.parametrize("nome", ["n@me", "Lu!z", "Jo@o", "Luis&", "$hirley", "marc*s"])
     def test_nome_nao_pode_conter_caracteres_especiais(self, nome):
         with pytest.raises(NomeInvalidoError) as target:
             self.criar_usuario(nome)
-        assert UsuarioValidacoes.NOME_NAO_PODE_TER_CARACTERES_ESPECIAIS.value in target.value.messages
+        assert UsuarioEnum.NOME_NAO_PODE_TER_CARACTERES_ESPECIAIS.value in target.value.messages
 
     @pytest.mark.parametrize("nome", ["An", "a", "zu"])
     def test_nome_nao_pode_conter_menos_de_3_caracteres(self, nome):
         with pytest.raises(NomeInvalidoError) as target:
             self.criar_usuario(nome)
-        assert UsuarioValidacoes.NOME_NAO_PODE_SER_MENOR_QUE_3_CHAR.value in target.value.messages
+        assert UsuarioEnum.NOME_NAO_PODE_SER_MENOR_QUE_3_CHAR.value in target.value.messages
 
     @pytest.mark.parametrize("nome", ["1@"])
     def test_nome_invalido_tamnanho_numero_simbolo(self, nome):
         lista_erros_esperados = []
         lista_erros_esperados.append(
-            UsuarioValidacoes.NOME_NAO_PODE_TER_NUMEROS.value)
+            UsuarioEnum.NOME_NAO_PODE_TER_NUMEROS.value)
         lista_erros_esperados.append(
-            UsuarioValidacoes.NOME_NAO_PODE_TER_CARACTERES_ESPECIAIS.value)
+            UsuarioEnum.NOME_NAO_PODE_TER_CARACTERES_ESPECIAIS.value)
         lista_erros_esperados.append(
-            UsuarioValidacoes.NOME_NAO_PODE_SER_MENOR_QUE_3_CHAR.value)
+            UsuarioEnum.NOME_NAO_PODE_SER_MENOR_QUE_3_CHAR.value)
 
         with pytest.raises(NomeInvalidoError) as target:
             self.criar_usuario(nome)
@@ -82,7 +83,7 @@ class TestUsuario():
         with pytest.raises(EmailInvalidoError) as target:
             self.criar_usuario(email=email)
         print(target.value.messages)
-        assert UsuarioValidacoes.EMAIL_NAO_PODE_COMECAR_OU_TERMINAR_COM_CARACTERES_ESPECIAIS.value in target.value.messages
+        assert UsuarioEnum.EMAIL_NAO_PODE_COMECAR_OU_TERMINAR_COM_CARACTERES_ESPECIAIS.value in target.value.messages
 
     """
     quadno email não tiver pelo menos um @
@@ -92,7 +93,7 @@ class TestUsuario():
     def test_email_deve_conter_arroba(self, email: str):
         with pytest.raises(EmailInvalidoError) as target:
             self.criar_usuario(email=email)
-        assert UsuarioValidacoes.EMAIL_DEVE_CONTER_ARROBA.value in target.value.messages
+        assert UsuarioEnum.EMAIL_DEVE_CONTER_ARROBA.value in target.value.messages
 
     """
     quando email tiver mais de um @
@@ -102,7 +103,7 @@ class TestUsuario():
     def test_email_nao_pode_conter_mais_de_um_arroba(self, email: str):
         with pytest.raises(EmailInvalidoError) as target:
             self.criar_usuario(email=email)
-        assert UsuarioValidacoes.EMAIL_NAO_PODE_CONTER_MAIS_QUE_UM_ARROBA.value in target.value.messages
+        assert UsuarioEnum.EMAIL_NAO_PODE_CONTER_MAIS_QUE_UM_ARROBA.value in target.value.messages
     """
     quuando o não existir um ponto apos o @
     entao deve ser gerado um erro E informar que email e invalido
@@ -111,7 +112,7 @@ class TestUsuario():
     def test_email_deve_conter_ponto_apos_arroba(self, email: str):
         with pytest.raises(EmailInvalidoError) as target:
             self.criar_usuario(email=email)
-        assert UsuarioValidacoes.EMAIL_DEVE_CONTER_PONTOS_APOS_ARROBA.value in target.value.messages
+        assert UsuarioEnum.EMAIL_DEVE_CONTER_PONTOS_APOS_ARROBA.value in target.value.messages
     """
     quando email não tiver ao menos 3 caracteres antes do @
     entao deve ser gerado um erro E informar que email e invalido
@@ -120,7 +121,7 @@ class TestUsuario():
     def test_email_deve_conter_3_caracteres_antes_do_arroba(self, email: str):
         with pytest.raises(EmailInvalidoError) as target:
             self.criar_usuario(email=email)
-        assert UsuarioValidacoes.EMAIL_DEVE_TER_PELO_MENOS_2_CARACTERES_ANTES_ARROBA.value in target.value.messages
+        assert UsuarioEnum.EMAIL_DEVE_TER_PELO_MENOS_2_CARACTERES_ANTES_ARROBA.value in target.value.messages
     """
     quando email não tiver ao menos 3 caracteres depois do @
     entao deve ser gerado um erro E informar que email e invalido
@@ -129,7 +130,7 @@ class TestUsuario():
     def test_email_deve_conter_3_caracteres_depois_do_arroba(self, email: str):
         with pytest.raises(EmailInvalidoError) as target:
             self.criar_usuario(email=email)
-        assert UsuarioValidacoes.EMAIL_DEVE_TER_PELO_MENOS_2_CARACTERES_DEPOIS_ARROBA.value in target.value.messages
+        assert UsuarioEnum.EMAIL_DEVE_TER_PELO_MENOS_2_CARACTERES_DEPOIS_ARROBA.value in target.value.messages
     """
     quando email estiver valido 
     então atributo email deve ser igual a email
